8eb12bd6d786b484b360870de469fe271c3a4543
ParkHyeonChae <park19996@naver.com>
2019-09-30 23:28:44 +0900

    user name 입력 후 유지

--- project/gretting.js
+++ project/gretting.js
@@ -2,8 +2,23 @@ const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
     greeting = document.querySelector(".js-greetings");
 
-const USER_LS = "currentUser",
-    SHOWING_CN = "showing";
+const USER_LS = "currentUser", SHOWING_CN = "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text); //사용자의 이름을 기억
+}
+
+function handleSubmit(event) {
+    event.preventDefault(); //default 이벤트를 막음
+    const currentValue = input.value; 
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit); //폼을 제출하는 이벤트 발생 시 handleSubmit 실행
+}
 
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
@@ -14,7 +29,7 @@ function paintGreeting(text){
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-
+        askForName(); //currentUser가 null이면 이름을 요청
     }
     else{
         paintGreeting(currentUser);
