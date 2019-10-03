4707c93d15dde74d5af6de08de53e22cc6b868d4
ParkHyeonChae <park19996@naver.com>
2019-10-03 21:05:31 +0900

    toDos local storage저장, forEach로 각각 호출

--- project/todo.js
+++ project/todo.js
@@ -4,15 +4,29 @@ const toDoform = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+const toDos = []; // 할일 저장
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); // local storage에 string으로 저장하기 위해 json사용
+}                               // JSON.stringify : js object를 string으로 변환
+
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.innerHTML = "❌";
     const span = document.createElement("span");
+    const newId = toDos.length +1; // local storage에도 저장하기 위해 neWId로 Id값 저장
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos()
 }
 
 function handleSubmit(event){
@@ -23,9 +37,12 @@ function handleSubmit(event){
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null) {
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){ // forEach : array 요소들에 각각 한번씩 함수 호출
+            paintToDo(toDo.text);
+        });
     }
  }
 
